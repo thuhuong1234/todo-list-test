e3afbf2cc150606a7da624a23750428ae2923451
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2025-06-24 23:29:24 +0700

    :sparkles: FE: Implement todo list page

---
+++ frontend/todos/src/components/PageHeader.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="action-toolbar">
+        <div class="toolbar-title">
+            {{ title }}
+        </div>
+        <div class="toolbar-buttons">
+            <slot name="btn" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<style scoped lang="scss">
+.action-toolbar {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 40px;
+    gap: 16px;
+
+    .toolbar-title {
+        font-size: 20px;
+        font-weight: bold;
+    }
+
+    .search-box {
+        display: flex;
+        align-items: center;
+        border: 1px solid #ccc;
+        border-radius: 6px;
+        padding: 6px 10px;
+        background-color: #fff;
+        width: 100%;
+        max-width: 300px;
+
+        .search-icon {
+            color: #666;
+            margin-right: 8px;
+        }
+
+        input {
+            border: none;
+            outline: none;
+            flex: 1;
+            font-size: 14px;
+        }
+    }
+
+    .toolbar-buttons {
+        display: flex;
+        gap: 12px;
+        flex-wrap: wrap;
+    }
+}
+</style>
--- frontend/todos/src/views/TodoList.vue
+++ frontend/todos/src/views/TodoList.vue
@@ -1,8 +1,35 @@
 <template>
-    <DefaultLayout>
-
-    </DefaultLayout>
+    <default-layout>
+        <div class="container">
+            <page-header title="Danh sách công việc">
+                <template #btn>
+                    <div class="btn"> <img src="@/assets/images/32-reload-3.svg" alt="Reload"></div>
+                    <div class="btn"> <img src="@/assets/images/32-c-add-3.svg" alt="Add"> Thêm công việc</div>
+                </template>
+            </page-header>
+        </div>
+    </default-layout>
 </template>
+
 <script setup>
 import DefaultLayout from '@/DefaultLayout.vue';
+import PageHeader from '@/components/PageHeader.vue';
 </script>
+
+<style lang="scss" scoped>
+.btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #cbcbcb;
+    background-color: #FFFFFF;
+    color: #2f6ab7;
+    border-radius: 7px;
+    padding: 8px;
+    font-weight: bold;
+    font-size: 14px;
+    gap: 10px;
+    cursor: pointer;
+}
+
+</style>
