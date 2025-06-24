8aa69519f189a30e327ee32f6a6d001261fb639a
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2025-06-24 21:47:03 +0700

    :sparkles: FE: Add option buttons

--- frontend/todos/src/App.vue
+++ frontend/todos/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AppHeader from "@/components/AppHeader.vue";
+import AppHeader from "@/components/Header.vue";
 import AppMain from "@/components/AppMain.vue";
 </script>
 
---
+++ frontend/todos/src/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+    <header>
+        <div class="app-header">
+            <div class="app-menu">
+                <img class="menu-btn" width="40px" src="@/assets/images/menu.svg" alt="Menu">
+                <div class="app-logo">
+                    <img class="logo" width="50%" src="@/assets/images/icode-logo.png" alt="Logo">
+                </div>
+            </div>
+            <div class="app-options">
+                <button class="btn-icon"> <img class="app-icon" width="20px" height="20px"
+                        src="@/assets/images/32-circle-arrow-left-3.svg" alt="Refresh"></button>
+                <button class="btn-icon"> <img class="app-icon" width="20px" height="20px"
+                        src="@/assets/images/32-mail-3.svg" alt="Mail"> </button>
+                <button class="btn-icon"> <img class="app-icon" width="20px" height="20px"
+                        src="@/assets/images/32-cogwheel-3.svg" alt="Setting"> </button>
+                <img class="avatar-account" width="40px" height="40px" src="@/assets/images/default-avatar.jpeg"
+                    alt="Account">
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.app-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 8px 40px;
+    background-color: white;
+
+    .app-menu {
+        display: flex;
+        gap: 50px;
+    }
+
+    .app-options {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 15px;
+
+        .btn-icon {
+            padding: 10px;
+            border-radius: 10px;
+            border: 1px solid #2F6AB7;
+            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+            cursor: pointer;
+            transition: background-color 0.2s;
+
+            .app-icon {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+        }
+
+        .btn-icon:hover {
+            background-color: #f0f0f0;
+        }
+
+        .avatar-account {
+            border-radius: 10px;
+        }
+    }
+}
+</style>
