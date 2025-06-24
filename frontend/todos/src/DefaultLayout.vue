fe512f3941fa93d3a31843d48cb15cfab9653b77
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2025-06-24 22:22:53 +0700

    :sparkles:  FE: Implement default layout

---
+++ frontend/todos/src/DefaultLayout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AppHeader from '@/components/Header.vue';
+import AppFooter from '@/components/Footer.vue';
+</script>
+
+<template>
+    <div class="default-layout">
+        <app-header />
+        <main class="app-main">
+            <slot />
+        </main>
+        <app-footer />
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+.default-layout {
+    width: 100%;
+    height: 100vh;
+    background: linear-gradient(#003DA1, #a2d6ec);
+}
+</style>
---
+++ frontend/todos/src/views/TodoList.vue
@@ -0,0 +1,8 @@
+<template>
+    <DefaultLayout>
+
+    </DefaultLayout>
+</template>
+<script setup>
+import DefaultLayout from '@/DefaultLayout.vue';
+</script>
