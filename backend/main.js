1a2d4a1294308d9856991aa4410c857ba55b329a
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2025-07-04 02:46:11 +0700

    :+1: Fix deploy fe file

--- backend/main.js
+++ backend/main.js
@@ -12,5 +12,5 @@ app.use("/api", todoRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
